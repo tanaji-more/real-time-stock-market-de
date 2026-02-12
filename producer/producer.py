8eb84801536aeab9b2f59815f3d7fd74ddfcf634
Tanaji More <tanajimore33@gmail.com>
2026-02-13 00:43:50 +0530

    updated code

--- producer/producer.py
+++ producer/producer.py
@@ -1,22 +1,22 @@
+#Import requirements
 import time
 import json
 import requests
 from kafka import KafkaProducer
 
 #Define variables for API
-
-API_KEY = "d6703vpr01qmckkbjgb0d6703vpr01qmckkbjgbg"
+API_KEY="d6703vpr01qmckkbjgb0d6703vpr01qmckkbjgbg"
 BASE_URL = "https://finnhub.io/api/v1/quote"
 SYMBOLS = ["AAPL", "MSFT", "TSLA", "GOOGL", "AMZN"]
 
+#Initial Producer
 producer = KafkaProducer (
-    bootstrap_servers = ["host.docker.internal:29092"],
-    value_serializer = lambda v: json.dumps(v).encode("utf-8") 
+    #bootstrap_servers=["host.docker.internal:29092"],
+    bootstrap_servers=["localhost:29092"],
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")  # convert dict into json file
 )
 
-
 #Retrive Data
-
 def fetch_quote(symbol):
     url = f"{BASE_URL}?symbol={symbol}&token={API_KEY}"
     try:
@@ -24,15 +24,17 @@ def fetch_quote(symbol):
         response.raise_for_status()
         data = response.json()
         data["symbol"] = symbol
-        data["fetched_at"] = int(time.time())
+        data["fetched_at"] = int (time.time())
         return data
     except Exception as e:
+        print(f"Error fetching {symbol}: {e}")
         return None
-    
+
+#Looping and Pushing to Stream
 while True:
     for symbol in SYMBOLS:
         quote = fetch_quote(symbol)
         if quote:
-            print(f"Producing : {quote}")
-            producer.send("stock-quotes", value = quote)
+            print(f"Producing: {quote}")
+            producer.send("stock-quotes", value=quote)
     time.sleep(6)
