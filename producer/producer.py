54f1fd943d155ac9d6f0b86ae337408bc9f7d929
Tanaji More <tanajimore33@gmail.com>
2026-02-12 23:03:23 +0530

    producer file code updated

--- producer/producer.py
+++ producer/producer.py
@@ -2,3 +2,33 @@ import time
 import json
 import requests
 from kafka import kafkaProducer
+
+
+API_KEY = "d6703vpr01qmckkbjgb0d6703vpr01qmckkbjgbg"
+BASE_URL = "https://finnhub.io/api/v1/quote"
+SYMBOLS = ["APPL", "MSFT", "TSLA", "GOOGL", "AMZN"]
+
+producer = kafkaProducer (
+    bootstrap_servers = ["host.docker.internal:29092"]
+    value_serializer = lambda v: json.dumps(v).encode("utf-8") 
+)
+
+def fecth_quote(symbol):
+    url = f"{BASE_URL}?symbol ={symbol}&token={API_KEY}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        data["symbol"] = symbol
+        data["fetched_at"] = int(time.time())
+        return data
+    except Exception as e:
+        return None
+    
+while True:
+    for symbol in SYMBOLS:
+        quote = fetch_quote(symbol)
+        if quote:
+            print(f"Producing : {quote}")
+            producer.send("stock-quotes", value = quote)
+        time.sleep(6)
