eb297cce8e6c2f83159cac84fa903dba80bd551e
Tanaji More <tanajimore33@gmail.com>
2026-02-13 11:16:31 +0530

    added gold layer

---
+++ dbt_stocks/dbt_stocks/models/gold/gold_kpi.sql
@@ -0,0 +1,11 @@
+SELECT
+    symbol,
+    current_price,
+    change_amount,
+    change_percent,
+FROM(
+SELECT *,
+ROW_NUMBER() OVER (PARTITION BY symbol ORDER BY fetched_at DESC) AS rn
+FROM {{ ref('silver_clean_stock_qoutes') }}
+)t 
+WHERE rn = 1
