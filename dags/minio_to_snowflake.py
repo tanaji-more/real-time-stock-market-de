748b7795e7c09881897037b59e31bcbb3b257fa4
Tanaji More <85829218+tanaji-more@users.noreply.github.com>
2026-02-13 14:21:55 +0530

    Update minio_to_snowflake.py

--- dags/minio_to_snowflake.py
+++ dags/minio_to_snowflake.py
@@ -11,8 +11,8 @@ MINIO_SECRET_KEY = 'password123'
 BUCKET = "bronze-transactions"
 LOCAL_DIR = "/tmp/minio_downloads"
 
-SNOWFLAKE_USER = "tanajimore"
-SNOWFLAKE_PASSWORD = "Snowflake@1122"
+SNOWFLAKE_USER = "snowflake username"
+SNOWFLAKE_PASSWORD = "Snowflake password"
 SNOWFLAKE_ACCOUNT = "ws92694.ap-southeast-1"
 SNOWFLAKE_WAREHOUSE = "COMPUTE_WH"
 SNOWFLAKE_DB = "STOCKS_MDS"
