3393f8bb62a77b424351c1e4fd05e846411da778
Tanaji More <tanajimore33@gmail.com>
2026-02-19 09:38:18 +0530

    updated changes

--- producer/producer.py
+++ producer/producer.py
@@ -13,7 +13,6 @@ SYMBOLS = ["AAPL", "MSFT", "TSLA", "GOOGL", "AMZN"]
 #Initial Producer
 producer = KafkaProducer (
     #bootstrap_servers=["host.docker.internal:29092"],
-    
     bootstrap_servers=["localhost:29092"],
     value_serializer=lambda v: json.dumps(v).encode("utf-8")  # convert dict into json file
 )
