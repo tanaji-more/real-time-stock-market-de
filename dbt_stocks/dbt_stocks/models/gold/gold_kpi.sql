9308a84b4d2175ccb4e6f15281c29fe7fad24994
Tanaji More <tanajimore33@gmail.com>
2026-02-19 09:44:50 +0530

    updated changes

--- dbt_stocks/dbt_stocks/models/gold/gold_kpi.sql
+++ dbt_stocks/dbt_stocks/models/gold/gold_kpi.sql
@@ -4,7 +4,6 @@ SELECT
     change_amount,
     change_percent
 FROM (
-    
     SELECT *,
            row_number() OVER (PARTITION BY symbol ORDER BY fetched_at DESC) AS rn
     FROM {{ ref('silver_clean_stock_quotes') }}
