c254130584e3a98dc401d7db3a5113c94b7fce5a
Tanaji More <tanajimore33@gmail.com>
2026-03-16 20:56:15 +0530

    Updated changes

--- dbt_stocks/dbt_stocks/models/bronze/bronze_stg_stock_quotes.sql
+++ dbt_stocks/dbt_stocks/models/bronze/bronze_stg_stock_quotes.sql
@@ -9,5 +9,6 @@ SELECT
     v:t::timestamp AS market_timestamp,
     v:symbol::string AS symbol,
     v:fetched_at::timestamp AS fetched_at
+   
     
 FROM {{source('raw', 'bronze_stock_quotes_raw')}}
